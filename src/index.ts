a7f60c1d1787c8249dbcca545d1305a961f191c3
Alfio <alfiopedrolivio.perino@nttdata.com>
2022-10-24 23:08:55 -0300

    addProduct service

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 8080
 
 import productosRouter from './routes/productos'
 
+// app.use('/',(_, res) => {
+//     res.status(200).json({
+//         health: "up",
+//         success: true
+//     })
+// })
+
 app.use('/api/productos', productosRouter)
 
 app.get('/ping', (_, res) => {
--- src/services/productosService.ts
+++ src/services/productosService.ts
@@ -1,23 +1,27 @@
 const fs = require('fs')
-import productsData from "./productos.json"
+import productsJson from "./productos.json"
 import { IProduct, IAddProduct } from "../types/types" 
 
-export const getAllProducts = (): IProduct[] => productsData as IProduct[]
+const productsData: IProduct[] = productsJson as IProduct[]
 
-export const getProductById = (id: number): IProduct | undefined => {
+export const getAllProducts = (): IProduct[] => productsData
+
+export const getProductById = (id: number): IProduct | string => {
     const findProduct = productsData.find(products => products.id === id)
-    return findProduct
+    return findProduct ? findProduct : `No se encuentra producto con el id ${id}`
 }
 
-export const addProduct = async ({title, price, thumbnail}: IAddProduct):Promise<IProduct> => {
+export const addProduct = async ({title, price, thumbnail}: IAddProduct): Promise<IProduct> => {
     try {
         const newProduct = {
             id: Math.max(...productsData.map(product => product.id)) +1,
             title, price, thumbnail
         }
-        const newProductData = productsData.push(newProduct)
-        await fs.promises.writeFile(`./productos.json`, JSON.stringify(newProductData))
-        return newProduct   
+          //  const parsedData = JSON.parse(productsData)
+            //const newProductData = productsData.push(newProduct)
+            console.log(productsData, newProduct)
+            // await fs.promises.writeFile('./src/services/productos.json', JSON.stringify(newProductData))
+            return newProduct   
     } catch (error) {
         throw new Error("No se pudo guardar el producto: " + title)
     }
@@ -26,8 +30,8 @@ export const addProduct = async ({title, price, thumbnail}: IAddProduct):Promise
 export const deleteProduct = async (id:number) => {
     try {
         const deletedProductsData = await productsData.filter(product => product.id != id)
-      
-        console.log(deletedProductsData)
+
+        //console.log(deletedProductsData)
         await fs.promises.writeFile(`./productos.json`, deletedProductsData)
         return id
     } catch (error) {
